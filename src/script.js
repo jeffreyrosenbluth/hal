aeecad4b7b441502c4129a889ca1a85d26f43e86
jeffreyrosenbluth <jeffrey.rosenbluth@gmail.com>
2022-07-02 10:29:08 -0400

    flux

--- src/script.js
+++ src/script.js
@@ -44,13 +44,13 @@ ballMaterial.matcap = goldMatcap
 
 const material = new THREE.MeshStandardMaterial()
 material.metalness = 1.0
-material.roughness = 0.1
+material.roughness = 0.0
 material.envMap = environmentMapTexture
 
-const saveButton = {
-    save: () => takeScreenshot(3000, 3000)
+const tweaks = {
+    save: () => takeScreenshot(3000, 3000),
+    cyl_length: 0.3,
 }
-
 
 const sphere = new THREE.Mesh(
     new THREE.SphereGeometry(0.82, 64, 64),
@@ -59,12 +59,33 @@ const sphere = new THREE.Mesh(
 sphere.geometry.setAttribute('uv2', new THREE.BufferAttribute(sphere.geometry.attributes.uv.array, 2))
 // sphere.position.x = - 1.5
 
-const ball = new THREE.Mesh(
-    new THREE.SphereGeometry(0.1, 64, 64),
+const cyl1 = new THREE.Mesh(
+    new THREE.CylinderGeometry(0.05, 0.04, tweaks.cyl_length, 64, 64),
     ballMaterial
 )
-ball.geometry.setAttribute('uv2', new THREE.BufferAttribute(sphere.geometry.attributes.uv.array, 2))
-ball.position.z = 0.8;
+cyl1.geometry.setAttribute('uv2', new THREE.BufferAttribute(sphere.geometry.attributes.uv.array, 2))
+cyl1.position.z = 0.78;
+cyl1.position.y = 0.22;
+
+const cyl2 = new THREE.Mesh(
+    new THREE.CylinderGeometry(0.04, 0.05,tweaks.cyl_length, 64, 64),
+    ballMaterial
+)
+cyl2.geometry.setAttribute('uv2', new THREE.BufferAttribute(sphere.geometry.attributes.uv.array, 2))
+cyl2.rotation.z = 0.8;
+cyl2.position.z = 0.78;
+cyl2.position.y = -0.16;
+cyl2.position.x = 0.18;
+
+const cyl3 = new THREE.Mesh(
+    new THREE.CylinderGeometry(0.04, 0.05, tweaks.cyl_length, 64, 64),
+    ballMaterial
+)
+cyl3.geometry.setAttribute('uv2', new THREE.BufferAttribute(sphere.geometry.attributes.uv.array, 2))
+cyl3.rotation.z = -0.8;
+cyl3.position.z = 0.78;
+cyl3.position.y = -0.16;
+cyl3.position.x = -0.18;
 
 const torus = new THREE.Mesh(
     new THREE.TorusGeometry(1.0, 0.1, 64, 128),
@@ -72,16 +93,16 @@ const torus = new THREE.Mesh(
 )
 torus.geometry.setAttribute('uv2', new THREE.BufferAttribute(torus.geometry.attributes.uv.array, 2))
 // torus.position.x = 1.5
-scene.add(sphere, ball, torus)
+scene.add(sphere, cyl1, cyl2, cyl3, torus)
 
-let sphereFolder = gui.addFolder('Sphere');
+// let sphereFolder = gui.addFolder('Sphere');
 // sphereFolder.add(sphere, 'scale', 0, 3.1415, 0.01).name('Radius');
 let ringFolder = gui.addFolder('Ring');
 ringFolder.add(material, 'metalness').min(0).max(1).step(0.0001).name('Reflectivity');
 ringFolder.add(material, 'roughness').min(0).max(1).step(0.0001).name('Roughness');
 ringFolder.add(torus.rotation, 'y', 0, 3.1415, 0.01).name('Vertical rotation')
 ringFolder.add(torus.rotation, 'x', 0, 3.1415, 0.01).name('Horizontal rotation')
-gui.add(saveButton, 'save')
+gui.add(tweaks, 'save')
 
 /**
  * Sizes
@@ -112,7 +133,7 @@ window.addEventListener('resize', () => {
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.x = 0
 camera.position.y = 0
-camera.position.z = 2
+camera.position.z = 1.75
 scene.add(camera)
 
 // Controls
@@ -185,7 +206,7 @@ function takeScreenshot(width, height) {
 
     // set camera and renderer to desired screenshot dimension
     camera.aspect = 1.0;
-    // camera.updateProjectionMatrix();
+    camera.updateProjectionMatrix();
     renderer.setSize(width, height);
 
     renderer.render(scene, camera, null, false);
